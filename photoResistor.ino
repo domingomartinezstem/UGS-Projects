6121894b3cbf778618b1a87ba5037d1da3c93a65
domingomartinezstem <30807066+domingomartinezstem@users.noreply.github.com>
2018-03-31 14:43:57 -0500

    Add files via upload

---
+++ photoResistor.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+Servo myservo;
+int button = 2;
+int val;
+int photocell;
+void setup() {
+  myservo.attach(9);
+  pinMode(button, INPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  photocell = analogRead(A0);
+  photocell = map(photocell, 0, 1023, 0, 180);
+  val = digitalRead(button);
+  if (val == 1) {
+    Serial.println(val);
+    myservo.write(70);
+    delay(10);
+  }
+  else {
+    myservo.write(photocell);
+    delay(10);
+    Serial.println(photocell);
+  }
+  delay(10);
+}
